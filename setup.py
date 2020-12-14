d2f049c9701499cec6d9edca73c035501f894717
Tristan Kuehn <tristankuehn@gmail.com>
2020-12-14 11:27:37 -0500

    Set up package properly

--- setup.py
+++ setup.py
@@ -1,3 +1,19 @@
-from setuptools import setup, find_packages
+"""Build script for cbsserverbilling."""
 
-setup(name="cbsserverbilling", packages=find_packages())
+import setuptools
+
+setuptools.setup(
+    name="cbsserverbilling",
+    version="1.0.0",
+    author="Tristan Kuehn",
+    author_email="tkuehn@uwo.ca",
+    description="A script to generate CBS Server bills.",
+    url="https://github.com/tkkuehn/cbs-server-billing",
+    packages=setuptools.find_packages(),
+    package_data={
+        "cbsserverbilling": ["templates/cbs_server_bill.tex.jinja"]
+    },
+    install_requires=[
+        "pandas",
+        "xlrd",
+        "Jinja2"])
